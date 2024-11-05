aafdd4a69ee210c03924f5190a8e8fb39e7b091c
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-11-05 20:05:00 +0530

    redux setup with userslice

---
+++ client/src/redux/Slices/userSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+        userInfo: null,
+        isLoggedIn: false,
+    },
+    reducers: {
+        setUser: (state, action) => {
+            state.userInfo = action.payload;
+            state.isLoggedIn = true;
+        },
+        logout: (state) => {
+            state.userInfo = null;
+            state.isLoggedIn = false;
+        },
+    },
+});
+
+export const { setUser, logout } = userSlice.actions;
+
+export default userSlice.reducer;
