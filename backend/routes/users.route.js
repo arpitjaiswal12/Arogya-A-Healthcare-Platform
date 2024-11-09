c7f89c14e12c1cae33f51b5c8134bb98dad3ed7f
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-11-09 10:41:05 +0530

    routers are added

---
+++ backend/routes/users.route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { signup, login, logout } = require('../controllers/Auth.js');
+const { updateDoctorProfile } = require('../controllers/User.js');
+
+router.post('/signup', signup);
+router.post('/login', login);
+router.get('/logout', logout);
+router.post('/update-profile/:id', updateDoctorProfile);
+
+module.exports = router;
