daf0c793fe66df2869f745068d131079e4e0dff7
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-11-10 18:21:10 +0530

    fix chatbot

--- client/src/components/ChatBox.js
+++ client/src/components/ChatBox.js
@@ -6,7 +6,7 @@ const ChatBox = () => {
     const script = document.createElement("script");
     script.innerHTML = `
       window.embeddedChatbotConfig = {
-        chatbotId: "s_NSIAHloWKzRjNuqu9kY",
+        chatbotId: "hvyvLHZop1MM-eTz9DwE9",
         domain: "www.chatbase.co"
       };
     `;
@@ -15,7 +15,7 @@ const ChatBox = () => {
     // Load Chatbase chatbot script
     const chatScript = document.createElement("script");
     chatScript.src = "https://www.chatbase.co/embed.min.js";
-    chatScript.chatbotId = "s_NSIAHloWKzRjNuqu9kY";
+    chatScript.chatbotId = "hvyvLHZop1MM-eTz9DwE9";
     chatScript.domain = "www.chatbase.co";
     chatScript.defer = true;
     document.body.appendChild(chatScript);
@@ -24,7 +24,7 @@ const ChatBox = () => {
   return (
     <div className="flex justify-center items-center w-full p-6 ">
       <iframe
-        src="https://www.chatbase.co/chatbot-iframe/s_NSIAHloWKzRjNuqu9kY"
+        src="https://www.chatbase.co/chatbot-iframe/hvyvLHZop1MM-eTz9DwE9"
         title="Chatbot"
         className="w-full max-w-4xl h-[700px] border-2 border-gray-300 rounded-lg shadow-lg"
         style={{ minHeight: "700px" }}
