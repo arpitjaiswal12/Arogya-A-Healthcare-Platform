1b93cbbf028dec2d7b04d4286476d4ecf1f72efe
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-12-02 17:39:35 +0530

    added private routes

--- client/src/components/ChatBox.js
+++ client/src/components/ChatBox.js
@@ -1,14 +1,12 @@
 import React, { useEffect } from "react";
 
 const ChatBox = () => {
-  console.log(process.env.REACT_APP_ChatBot_Id)
-  console.log(process.env.REACT_APP_BACKEND_URL)
   useEffect(() => {
     // Embed Chatbase chatbot script
     const script = document.createElement("script");
     script.innerHTML = `
       window.embeddedChatbotConfig = {
-        chatbotId: ${process.env.REACT_APP_ChatBot_Id},
+        chatbotId: "hvyvLHZop1MM-eTz9DwE9",
         domain: "www.chatbase.co"
       };
     `;
@@ -17,7 +15,7 @@ const ChatBox = () => {
     // Load Chatbase chatbot script
     const chatScript = document.createElement("script");
     chatScript.src = "https://www.chatbase.co/embed.min.js";
-    chatScript.chatbotId = `${process.env.REACT_APP_ChatBot_Id}`;
+    chatScript.chatbotId = "hvyvLHZop1MM-eTz9DwE9";
     chatScript.domain = "www.chatbase.co";
     chatScript.defer = true;
     document.body.appendChild(chatScript);
@@ -26,7 +24,7 @@ const ChatBox = () => {
   return (
     <div className="flex justify-center items-center w-full p-6 ">
       <iframe
-        src={`https://www.chatbase.co/chatbot-iframe/${process.env.REACT_APP_ChatBot_Id}`}
+        src="https://www.chatbase.co/chatbot-iframe/hvyvLHZop1MM-eTz9DwE9"
         title="Chatbot"
         className="w-full max-w-4xl h-[700px] border-2 border-gray-300 rounded-lg shadow-lg"
         style={{ minHeight: "700px" }}
--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,11 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import {Navigate} from 'react-router-dom';
 
 const PrivateRoute = ({isLoggedIn, children}) => {
-  if(isLoggedIn) {
-    return children;
-  }
-  else {
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+  if(!currentUser){
     return <Navigate to="/login"/>
   }
 }
