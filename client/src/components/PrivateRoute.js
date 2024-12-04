ca4edfcbf4870461037dcdd50cf947e619f3c688
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-12-05 00:09:33 +0530

    added private route

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 import {Navigate} from 'react-router-dom';
 
-const PrivateRoute = ({isLoggedIn, children}) => {
-  const { currentUser, loading, error } = useSelector((state) => state.user);
+const PrivateRoute = ({children}) => {
+  const { currentUser} = useSelector((state) => state.user);
   if(!currentUser){
     return <Navigate to="/login"/>
   }
-  else{
-    return <Navigate to="/profile"/>
-  }
+
+  return children;
+
 }
 
 export default PrivateRoute
